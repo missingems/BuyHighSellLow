12a59c79a71c48817dd249e37bc888c8adea4c87
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-01 01:08:33 +0800

    Simplify Order Book
    Initial concern was data could be huge and process takes long. But realised we can set depth to 25 when querying entries

--- BuyHighSellLow/Model/OrderBook.swift
+++ BuyHighSellLow/Model/OrderBook.swift
@@ -19,92 +19,46 @@ final class OrderBook {
   }
   
   private(set) var sides = Sides()
+  private var entries: [OrderBookEntry] = []
   
-  init(_ message: WebSocketMessage<OrderBookEntry>) async throws {
+  init(_ message: WebSocketMessage<OrderBookEntry>) throws {
     guard message.action == .partial else {
       throw OrderBookError.orderBookInitialiseFromNonPartial
     }
     
-    sides = await update(with: message, existingSides: sides)
+    update(with: message)
   }
   
-  func send(newMessage: WebSocketMessage<OrderBookEntry>) async {
-    sides = await update(with: newMessage, existingSides: sides)
+  func send(newMessage: WebSocketMessage<OrderBookEntry>) {
+    update(with: newMessage)
   }
   
-  private func update(
-    with newMessage: WebSocketMessage<OrderBookEntry>,
-    existingSides: Sides
-  ) async -> OrderBook.Sides {
-    return await withCheckedContinuation { continuation in
-      DispatchQueue.global().async {
-        switch newMessage.action {
-        case .delete:
-          var copiedBids = existingSides.bids
-          var copiedAsks = existingSides.asks
-          
-          for data in newMessage.data {
-            copiedBids.removeAll { $0.id == data.id }
-            copiedAsks.removeAll { $0.id == data.id }
-          }
-          
-          let newSides = Sides(bids: copiedBids, asks: copiedAsks)
-          continuation.resume(returning: newSides)
-          
-        case .insert:
-          var copiedBids = existingSides.bids
-          var copiedAsks = existingSides.asks
-          
-          for data in newMessage.data {
-            switch data.side {
-            case .buy:
-              if let index = copiedAsks.firstIndex(where: { data.price >= $0.price }){
-                copiedBids.insert(data, at: index)
-              } else {
-                copiedBids.append(data)
-              }
-              
-            case .sell:
-              if let index = copiedAsks.firstIndex(where: { data.price <= $0.price }) {
-                copiedAsks.insert(data, at: index)
-              } else {
-                copiedAsks.append(data)
-              }
-            }
-          }
-          
-          let newSides = Sides(bids: copiedBids, asks: copiedAsks)
-          continuation.resume(returning: newSides)
-          
-        case .partial:
-          let bids = newMessage.data.filter { $0.side == .buy }.sorted { $0.price > $1.price }
-          let asks = newMessage.data.filter { $0.side == .sell }.sorted { $0.price < $1.price }
-          let newSides = Sides(bids: bids, asks: asks)
-          continuation.resume(returning: newSides)
-          
-        case .update:
-          var copiedBids = existingSides.bids
-          var copiedAsks = existingSides.asks
-          
-          for data in newMessage.data {
-            switch data.side {
-            case .buy:
-              if let index = copiedBids.firstIndex(where: { $0.id == data.id }) {
-                copiedBids[index] = data
-              }
-              
-            case .sell:
-              if let index = copiedAsks.firstIndex(where: { $0.id == data.id }) {
-                copiedAsks[index] = data
-              }
-            }
-          }
-          
-          let newSides = Sides(bids: copiedBids, asks: copiedAsks)
-          continuation.resume(returning: newSides)
+  private func update(with newMessage: WebSocketMessage<OrderBookEntry>) {
+    switch newMessage.action {
+    case .delete:
+      for data in newMessage.data {
+        entries.removeAll(where: { $0.id == data.id })
+      }
+      
+    case .insert:
+      entries.append(contentsOf: newMessage.data)
+      
+    case .partial:
+      entries = newMessage.data
+      
+    case .update:
+      for entry in newMessage.data {
+        guard let index = entries.firstIndex(where: { $0.id == entry.id }) else {
+          return
         }
+        
+        entries[index] = entry
       }
     }
+    
+    let bids = Array(entries.filter { $0.side == .buy }.sorted { $0.price > $1.price }.prefix(20))
+    let asks = Array(entries.filter { $0.side == .sell }.sorted { $0.price < $1.price }.prefix(20))
+    sides = Sides(bids: bids, asks: asks)
   }
 }
 
