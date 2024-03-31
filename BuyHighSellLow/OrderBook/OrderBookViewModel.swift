7625dee504784324f22be623a705251d103d56f5
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-03-31 23:26:20 +0800

    Update OrderBookViewModel.swift

--- BuyHighSellLow/OrderBook/OrderBookViewModel.swift
+++ BuyHighSellLow/OrderBook/OrderBookViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import Starscream
+
+@Observable
+final class OrderBookViewModel {
+  var sides: OrderBook.Sides
+  
+  init() {
+    sides = OrderBook.Sides()
+  }
+  
+  func update(_ action: Action) {
+    switch action {
+    case .viewAppeared:
+      break
+      
+    case .viewDisappeaered:
+      break
+    }
+  }
+}
+
+extension OrderBookViewModel {
+  enum Action {
+    case viewAppeared
+    case viewDisappeaered
+  }
+}
