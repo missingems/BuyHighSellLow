5461137e513ed430e53e4dfb567b22480c2a4694
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-03-31 23:10:30 +0800

    Update OrderBookEntry.swift

--- BuyHighSellLow/Model/OrderBookEntry/OrderBookEntry.swift
+++ BuyHighSellLow/Model/OrderBookEntry/OrderBookEntry.swift
@@ -6,11 +6,39 @@
 //
 
 struct OrderBookEntry: Decodable, Identifiable, Equatable {
+  enum CodingKeys: String, CodingKey {
+    case id, price, side, size, symbol
+  }
+  
   let id: Int
-  let price: Double?
+  let price: Double
   let side: Side
   let size: Int?
   let symbol: String
+  
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    
+    id = try container.decode(Int.self, forKey: .id)
+    price = try container.decodeIfPresent(Double.self, forKey: .price) ?? 0
+    side = try container.decode(Side.self, forKey: .side)
+    size = try container.decodeIfPresent(Int.self, forKey: .size)
+    symbol = try container.decode(String.self, forKey: .symbol)
+  }
+  
+  init(
+    id: Int,
+    price: Double = 0,
+    side: OrderBookEntry.Side,
+    size: Int? = nil,
+    symbol: String
+  ) {
+    self.id = id
+    self.price = price
+    self.side = side
+    self.size = size
+    self.symbol = symbol
+  }
 }
 
 extension OrderBookEntry {
