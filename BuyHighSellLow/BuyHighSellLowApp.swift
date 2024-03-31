9e8902bffca9ae5f006a626b6b982f35a183c515
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-03-31 20:35:20 +0800

    Initial Commit

---
+++ BuyHighSellLow/BuyHighSellLowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuyHighSellLowApp.swift
+//  BuyHighSellLow
+//
+//  Created by Jun on 31/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BuyHighSellLowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
