1e810f333c73efc3857cd904f02238303966353d
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-01 06:38:37 +0800

    Update Trade.swift

--- BuyHighSellLow/Model/Trade/Trade.swift
+++ BuyHighSellLow/Model/Trade/Trade.swift
@@ -7,38 +7,54 @@
 
 import Foundation
 
-struct Trade: Decodable, Equatable {
+struct Trade: Decodable, Equatable, Identifiable {
   enum CodingKeys: String, CodingKey {
-    case timestamp, side, size, price, tickDirection
+    case timestamp, side, size, price, tickDirection, trdMatchID
   }
   
+  let id: String?
   let timestamp: String
   var displaydate: Date?
   let side: Side?
   let size: Double?
-  let price: Double?
+  let price: Double
   let tickDirection: TickDirection?
+  
+  var displayPrice: String {
+    String(format: "%.1f", price)
+  }
+  
+  var displaySize: String? {
+    if let size {
+      return String(format: "%.4f", size / price)
+    } else {
+      return nil
+    }
+  }
   
   init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
+    id = try? container.decodeIfPresent(String.self, forKey: .trdMatchID) ?? UUID().uuidString
     timestamp = try container.decode(String.self, forKey: .timestamp)
-    side = try container.decode(Side.self, forKey: .side)
-    size = try container.decode(Double.self, forKey: .size)
-    price = try container.decode(Double.self, forKey: .price)
-    tickDirection = try container.decode(TickDirection.self, forKey: .tickDirection)
+    side = try? container.decodeIfPresent(Side.self, forKey: .side)
+    size = try? container.decodeIfPresent(Double.self, forKey: .size)
+    price = (try? container.decodeIfPresent(Double.self, forKey: .price)) ?? 0
+    tickDirection = try? container.decodeIfPresent(TickDirection.self, forKey: .tickDirection)
     
-    let formatter = ISO8601DateFormatter()
+    let dateFormatter = ISO8601DateFormatter()
+    dateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
     
-    if let timestampDate = formatter.date(from: timestamp) {
-      displaydate = timestampDate
+    if let date = dateFormatter.date(from: timestamp) {
+      displaydate = date
     }
   }
   
   init(
+    id: String,
     timestamp: String,
     side: Trade.Side? = nil,
     size: Double? = nil,
-    price: Double? = nil,
+    price: Double = 0,
     tickDirection: Trade.TickDirection? = nil
   ) {
     self.timestamp = timestamp
@@ -52,6 +68,12 @@ struct Trade: Decodable, Equatable {
     if let timestampDate = formatter.date(from: timestamp) {
       displaydate = timestampDate
     }
+    
+    self.id = id
+  }
+  
+  static func == (lhs: Self, rhs: Self) -> Bool {
+    return lhs.id == rhs.id
   }
 }
 
@@ -68,5 +90,13 @@ extension Trade {
     case zeroPlusTick = "ZeroPlusTick"
     case minusTick = "MinusTick"
     case zeroMinusTick = "ZeroMinusTick"
+  }
+}
+
+extension Date {
+  func twentyHoursTimeString() -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "HH:mm:ss"
+    return dateFormatter.string(from: self)
   }
 }
